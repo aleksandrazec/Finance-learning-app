943335e9a5c5d774cba7724deca320fe6773db5a
Filip Trajkoski <89231028@student.upr.si>
2025-11-23 04:19:08 +0100

    Added a post /stock for adding a single stock

--- back-end/routes/stock.js
+++ back-end/routes/stock.js
@@ -7,6 +7,29 @@ const db = require('../DB/DbConn.js')
 var bodyParser = require('body-parser')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
+stock.post('/', urlencodedParser, async (req, res) => {
+    try {
+
+        const date=req.body.date
+        const close=req.body.close
+        const volume=req.body.volume
+        const company=req.body.company
+
+        const queryResult = await db.addStock(date, close, volume, company)
+        if (queryResult.affectedRows) {
+            res.json({ status: { success: true, message: "Successfully added a stock!" } })
+        } else {
+            res.json({ status: { success: false, message: "Error while adding a stock!" } })
+        }
+    } catch (err) {
+        console.log("Error in post /stock/ route")
+
+    }
+})
+
+
+
 stock.post('/batch', urlencodedParser, async (req, res) => {
     try {
         // receiving text
